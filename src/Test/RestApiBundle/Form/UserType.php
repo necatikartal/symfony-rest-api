cf5252962f4c2d9722eadb40c72cff15f8f8f3de
necatikartal <nctkrtl@hotmail.com>
2014-07-09 12:10:47 +0300

    added usertype form

---
+++ src/Test/RestApiBundle/Form/UserType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Test\RestApiBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('userpasswod')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Test\RestApiBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'test_restapibundle_user';
+    }
+}
